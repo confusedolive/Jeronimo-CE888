14f2fd00007bcbc41572b65c91367c186025cdbc
confusedolive <jeronimomarbella@gmail.com>
2021-03-31 12:12:28 +0100

    changed correlation.py

--- assignment02/correlation.py
+++ assignment02/correlation.py
@@ -1,8 +1,14 @@
-import pandas as pd
-from scipy.stats import linregress, spearmanr
-import numpy as np
-import matplotlib.pyplot as plt
 import os
+import pandas  as pd
+import seaborn as sns
+import numpy   as np
+
+import matplotlib.pyplot as plt
+
+from scipy.stats           import linregress, spearmanr
+from sklearn.cluster       import  KMeans
+from sklearn.preprocessing import  StandardScaler
+
 
 fig_path = os.path.join(os.getcwd(), 'visuals')
 df = pd.read_csv(r'data\filtered_merged_data_2019.csv')
@@ -31,6 +37,7 @@ grouped = [[brgh, len(ds),
 new_df = pd.DataFrame(grouped, columns=['borough','stops','median income',
                                         'arrests','unemployment'])
 
+##########visualize linear relationships########################
 def line_int(var1 , var2):
     line = linregress(new_df[var1], new_df[var2])
     plt.plot(new_df[var1], new_df[var2], 'o', label='data')
@@ -47,3 +54,35 @@ def line_int(var1 , var2):
 
 for x in ['stops','median income', 'unemployment']:
     line_int('arrests', x )
+
+
+#################k means clusterign to find groups among unemployment and median household income###############
+data_k = data.iloc[:,-2:].copy()
+
+standard = StandardScaler()
+transform = data_k.copy()
+data_standard = standard.fit_transform(transform)
+
+#elbow chart to find number of clusters
+def elbowing():
+    wcss = []
+    for i in range(1,11):
+        kmean = KMeans(n_clusters=i)
+        kmean.fit(data_standard)
+        wcss.append(kmean.inertia_)
+
+    plt.plot(range(1,11), wcss, marker='o', linestyle='-')
+    plt.show()
+
+#n clusters = 3
+kmean = KMeans(n_clusters=3)
+kmean.fit(test2)
+
+data_k['labels']=kmean.labels_
+data_k.dropna(inplace=True)
+
+sns.scatterplot(x=data_k['median income'],
+                y=data_k['unemployment rate'],
+                hue=data_k['labels'])
+
+plt.savefig(os.path.join(fig_path,'scatter_test_kmeans'))
